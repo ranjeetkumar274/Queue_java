bc0b272a2ea627d3264c21289b419617b274609c
ranjeetkumar274 <2023073045@mmmut.ac.in>
2024-09-30 18:06:12 +0530

    Reverse the Queue Using Stack

--- reverseQueueUsingStack.java
+++ reverseQueueUsingStack.java
@@ -1,13 +1,7 @@
 import java.util.*;
 public class reverseQueueUsingStack {
-    public static void main(String[] args){
-        Queue<Integer> que = new LinkedList<>();
-        Stack<Integer> st = new Stack<>();
 
-        for(int i = 0; i < 10; i++){
-            que.add(i+1);
-        }
-
+    static Queue<Integer> reverse(Queue<Integer> que, Stack<Integer> st){
         System.out.println(que);
         while(!que.isEmpty()){
             st.add(que.poll());
@@ -16,7 +10,16 @@ public class reverseQueueUsingStack {
         while(!st.isEmpty()){
             que.add(st.pop());
         }
+        return que;
+    }
+    public static void main(String[] args){
+        Queue<Integer> que = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
 
-        System.out.println(que);
+        for(int i = 0; i < 10; i++){
+            que.add(i+1);
+        }
+
+        System.out.println(reverse(que,st));
     }
 }
